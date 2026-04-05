1cfed4f4c2c6dd475341eca07deee1f93d5edd6a
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-05 19:16:30 +0300

    nav

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Nav = () => {
    const [open, setOpen] = useState(false)
+   const {currentUser} = useSelector((state)=>state.user)
+   const navigate  = useNavigate()
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
@@ -11,13 +15,20 @@ const Nav = () => {
 
             {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
-                <a href="#">Home</a>
-                <a href="#">About</a>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
                 <a href="#">Contact</a>
 
-                <button className="cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
+              {
+                currentUser == null || !currentUser ? 
+                <button onClick={()=>navigate('/login')} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
                     Login
                 </button>
+                :
+                <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
+                    Logout
+                </button>
+              }
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
@@ -31,12 +42,19 @@ const Nav = () => {
 
             {/* Mobile Menu */}
             <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
-                <a href="#" className="block">Home</a>
-                <a href="#" className="block">About</a>
+                <a href="/" className="block">Home</a>
+                <a href="/about" className="block">About</a>
                 <a href="#" className="block">Contact</a>
-                <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
+              {
+                currentUser == null || !currentUser ? 
+                <button onClick={()=>navigate('/login')} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
                     Login
                 </button>
+                :
+                <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
+                    Logout
+                </button>
+              }
             </div>
 
         </nav>
