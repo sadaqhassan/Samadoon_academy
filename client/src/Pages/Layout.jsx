e3d499258fd8637978104fa4b92a79ffaecc1e51
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-05 18:46:29 +0300

    layout

--- client/src/Pages/Layout.jsx
+++ client/src/Pages/Layout.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Side from '../Components/Side'
+import { Outlet } from 'react-router-dom'
+import { MenuIcon } from 'lucide-react'
 
 const Layout = () => {
+  const [isOpen,setIsOpen] = useState(false)
   return (
-    <div className='flex'>
-        <Side/>
+    <div className='flex space-x-5 items-start bg-cyan-50 max-w-full h-screen overflow-y-hidden'>
+        {
+          isOpen && <Side/>
+        }  
+        <button className={`${!isOpen && "ml-8"}  mt-5`} onClick={()=>setIsOpen(!isOpen)}><MenuIcon/></button>
+        <div className='mt-4 ml-5 flex-wrap max-w-full h-screen overflow-y-scroll'>
+          <Outlet/>
+        </div>
     </div>
   )
 }
