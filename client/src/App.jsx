116a2aa11139b3b079eec1e44e24acec30075371
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-05 18:46:03 +0300

    path and scroll

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Layout from './Pages/Layout'
+import Teachers from './Pages/Teachers'
+import Students from './Pages/Students'
+import Fees from './Pages/Fees'
+import Dashboard from './Pages/Dashboard'
+import { useSelector } from 'react-redux'
+import Attendence from './Pages/Attendence'
+import Nav from './Components/Nav'
 
 function App() {
+  const currentUser = useSelector((state)=>state?.user.currentUser)
   return (
     <div>
-
       <Routes>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/' element={<Layout/>}/>
+        <Route  path='/' element={!currentUser || currentUser == null ? <Home/> : <Layout/>}>
+        <Route path='dashboard' element={<Dashboard/>}/>
+        <Route path='teachers' element={<Teachers/>}/>
+        <Route path='students' element={<Students/>}/>
+        <Route path='finance' element={<Fees/>}/>
+        <Route path='attendence' element={<Attendence/>}/>
+        </Route>
       </Routes>
     </div>
   )
