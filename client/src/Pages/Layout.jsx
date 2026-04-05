3204f757688d35857323b52a69eca3029f2d35f6
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-05 14:12:12 +0300

    layout

---
+++ client/src/Pages/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Side from '../Components/Side'
+
+const Layout = () => {
+  return (
+    <div className='flex'>
+        <Side/>
+    </div>
+  )
+}
+
+export default Layout
