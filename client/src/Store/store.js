f7b5e26c55db2b8175dd744c005c32629161321d
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-05 14:12:18 +0300

    userSlice

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './userSlice';
+
 
 export const store = configureStore({
-    reducer:{}
+    reducer:{
+        theUser:userReducer
+    }
 })
