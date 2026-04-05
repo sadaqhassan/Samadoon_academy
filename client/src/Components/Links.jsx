d51488096569f546f5b845f932279dfc39f6de49
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-05 18:46:08 +0300

    Links

--- client/src/Components/Links.jsx
+++ client/src/Components/Links.jsx
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom'
 
 const Links = () => {
   return (
-    <div className='flex flex-col space-y-4 ml-5'>
-        <NavLink to={'/'} className={(isActive)=>`${isActive &&"bg-green-400 px-2 py-2"}`}>Dashboard</NavLink>
-        <NavLink to={'/teacher'} >Teachers</NavLink>
-        <NavLink to={'/students'} >Students</NavLink>
-        <NavLink to={'/finance'} >Finance</NavLink>
-        <NavLink to={'/attendence'} >Attendence</NavLink>
+    <div className='flex flex-col space-y-4  ml-0'>
+        <NavLink to={'/'} className={({isActive})=>`${isActive &&"bg-green-700 px-2 py-2 rounded"}`}>Dashboard</NavLink>
+        <NavLink  className={({isActive})=>`${isActive &&"bg-green-700 px-2 py-2 rounded"}`}  to={'teachers'} >Teachers</NavLink>
+        <NavLink className={({isActive})=>`${isActive &&"bg-green-700 px-2 py-2 rounded"}`} to={'students'} >Students</NavLink>
+        <NavLink className={({isActive})=>`${isActive &&"bg-green-700 px-2 py-2 rounded"}`} to={'finance'} >Finance</NavLink>
+        <NavLink className={({isActive})=>`${isActive &&"bg-green-700 px-2 py-2 rounded"}`} to={'attendence'} >Attendence</NavLink>
     </div>
   )
 }
