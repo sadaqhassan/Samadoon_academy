a1c0a44b5d47e6f003fb712cf98cbfeb5d3e8f64
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-06 22:15:18 +0300

    ed

--- Server/server.js
+++ Server/server.js
@@ -1,26 +1,25 @@
 import express from 'express'
 import { configApp } from './utils/Config.js';
-import connectionDb from './utils/conn.js';
-import { userRoute } from './Routes/user.route.js';
 import bcrypt from 'bcryptjs';
 import { theUser } from './models/user.model.js';
+import { userRoute } from './Routes/user.route.js';
+import connectionDb from './utils/conn.js';
 const app = express();
 
 configApp(app)
 
 app.use("/api/user",userRoute);
 
-connectionDb()
 
-
+await connectionDb();
 
 const existingAdmin = await theUser.findOne({ role: "admin" });
 
 if (!existingAdmin) {
     const hashed =  bcrypt.hashSync(process.env.AdminPassword, 10);
 
-await User.create({
-    name: "Super Admin",
+await theUser.create({
+    username: "Super Admin",
     email: "admin@gmail.com",
     password: hashed,
     role: "admin",
