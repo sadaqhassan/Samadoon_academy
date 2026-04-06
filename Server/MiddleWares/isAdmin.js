b02ac6dba787747d1bfe12259200b4a9d55f2a58
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-06 22:15:42 +0300

    admin

---
+++ Server/MiddleWares/isAdmin.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+
+export const isAdminM = async (req,res,next) => {
+    const token = req.cookies.accessToken;
+
+    if(!token) return res.status(401).jspn({success:false,message:"You are not authed"})
+
+    const isVerified = await jwt.verify(token,process.env.JWT_SECRET);
+    
+    if(!isVerified) return res.status(403).jspn({success:false,message:"You are not authorized"});
+
+    req.userRole = "admin",
+
+    next()
+
+}
