a0753ecd13aefbf0b6b3e6cf2c1f25b371633dfe
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-06 11:56:50 +0300

    user model

---
+++ Server/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userModel = mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    avatar:{
+        type:String,
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjEofX99GPaBBLF9NPxmpiDRHAmvY00shDnw&s"
+    },
+    role:{
+        type:String,
+        required:true,
+        enum:["admin,teacher"]
+    }
+},{timestamps:true})
+
+export const theUser = mongoose.model("user",userModel);
