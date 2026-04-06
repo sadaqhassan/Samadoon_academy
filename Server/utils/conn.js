4c491a9ac349da180d7ade7b13e6b5b86ed09647
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-06 22:16:00 +0300

    conn

--- Server/utils/conn.js
+++ Server/utils/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectionDb = async () => {
     const db = process.env.DBSTRING
-    mongoose.connect(db).then(()=>console.log("connected to the db")).catch((err)=>console.log(err.message))
+    await mongoose.connect(db).then(()=>console.log("connected to the db")).catch((err)=>console.log(err.message))
 }
 
 export default connectionDb;
