a636dda6ef68de52af918dc7f7b00fe73bf0ca20
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-07 14:08:26 +0300

    fixed and completed register teaacher

--- Server/Controllers/teacher.controller.js
+++ Server/Controllers/teacher.controller.js
@@ -1,18 +1,25 @@
-import { teacherModel } from "../models/teacher.model";
+import { teacherModel } from "../models/teacher.model.js";
 
 export const teacherRegister  = async (req,res) => {
     const {phone_number,teachers_email,teachers_name} = req.body;
-    
-    if(!phone_number || !teachers_email || !teachers_name) return res.status(400).json({Success:false,message:"fill al fields"})
+
+    if(!phone_number || !teachers_email || !teachers_name) return res.status(400).json({success:false,message:"fill al fields"})
     try {
-        const teacher = await teacherModel.findOne({email:teachers_email});
-        if(!teacher) {
-            return res.status(400).json({Success:false,message:"teahcer is exist"});
+        const teacher = await teacherModel.findOne({teachers_email});
+        if(teacher) {
+            return res.status(400).json({success:false,message:"teacher is exist"});
         };
 
-        await teacherModel.create(req.body);
+        const count = await teacherModel.countDocuments();
+        
+        await teacherModel.create({
+            phone_number,
+            teachers_email,
+            teachers_name,
+            id:count+1
+        });
 
-        return res.status(400).json({Success:false,message:"teacher Register successFully"});
+        return res.status(200).json({success:true,message:"teacher Register successFully"});
     } catch (error) {
         res.status(500).json({Success:false,message:"server errror"})
         console.log(error);
