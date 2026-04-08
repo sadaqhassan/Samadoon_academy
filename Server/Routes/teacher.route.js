cb853d27762941b97ff713a9cea62f915d124b5d
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-08 18:45:48 +0300

    delete teacher route

--- Server/Routes/teacher.route.js
+++ Server/Routes/teacher.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { teacherInfo, teacherRegister } from "../Controllers/teacher.controller.js";
+import { deleteTecherApi, teacherInfo, teacherRegister } from "../Controllers/teacher.controller.js";
 import { isAdminM } from "../MiddleWares/isAdmin.js";
 
 const teacher = Router();
 
-teacher.post("/register",teacherRegister);
+teacher.post("/register",isAdminM,teacherRegister);
 teacher.get("/get",isAdminM,teacherInfo);
+teacher.get("/get",isAdminM,deleteTecherApi);
 
 export const teacherRoute = teacher;
