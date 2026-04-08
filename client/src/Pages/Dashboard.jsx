1c20c3779a8179288fcbe1f4ed143ede09a61ff3
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-08 17:18:30 +0300

    some fixed

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { DollarSign, User2 } from 'lucide-react'
+import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
+    const {teachers} = useSelector((state)=>state.teachers)
   const [dashboardUser,setDashboardUser] = useState([
-    {name:"Teachers",count:34,color:"green-600",icon:<User2/>},
+    {name:"Teachers",count:teachers?.length,color:"green-600",icon:<User2/>},
     {name:"Students",count:1423,color:"cyan-600",icon:<User2/>},
-    {name:"MonthlyIncome",count:"$9539",color:"gray-300",icon:<DollarSign/>}
+    {name:"MonthlyIncome",count: 4443,color:"gray-300",icon:<DollarSign/>}
   ]);
   return (
     <div className='flex flex-wrap gap-5 mt-10 w-full '>
@@ -14,9 +16,9 @@ const Dashboard = () => {
           <div className={`${dash.name === "Teachers" ? "bg-green-600" : dash.name === "Students" ? "bg-cyan-600" : "bg-gray-400"} p-5 w-76 text-white  rounded`}>
             <div className='flex justify-between px-2 flex-wrap'>
               <p>{dash.name}</p>
-              <p>{dash.icon}</p>
+              <p className=''>{dash.icon}</p>
             </div>
-            <p className='text-center mt-1'>{dash.count}</p>
+            <p className='text-xl font-bold  text-center mt-1'>{dash.count}</p>
           </div>
         ))
       }
