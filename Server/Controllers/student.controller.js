3a82a5b8ba5bd3b0d43fbf75ec0874b21af18554
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-11 10:28:23 +0300

    studentController

--- Server/Controllers/student.controller.js
+++ Server/Controllers/student.controller.js
@@ -1,32 +1,32 @@
 import { studentModel } from "../models/student.model.js";
 
 export const studentRegister  = async (req,res) => {
-    const {phone_number,name} = req.body;
+    const {phone_number,name,Grade} = req.body;
 
     if(!phone_number || !name) return res.status(400).json({success:false,message:"fill al fields"})
     try {
-        const student = await studentModel.findOne({student_email});
+        const student = await studentModel.findOne({phone_number});
         if(student) {
-            return res.status(400).json({success:false,message:"=student is exist"});
+            return res.status(400).json({success:false,message:"student is exist"});
         };
 
         const count = await studentModel.countDocuments();
         
         await studentModel.create({
             phone_number,
-            email,
+            Grade,
             name,
-            StudentId:count+1
+            studentId:count+1
         });
 
-        return res.status(200).json({success:true,message:"=student Register successFully"});
+        return res.status(200).json({success:true,message:"student Register successFully"});
     } catch (error) {
-        res.status(500).json({Success:false,message:"server errror"})
+        res.status(500).json({Success:false,message:error})
         console.log(error);
     }
 }
 
-// http://localhost:2000/api/student/register/get
+// http://localhost:2000/api/student/get
 
 export const studentInfo = async (req,res) => {
     const {userRole} = req
